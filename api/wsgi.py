8130309cd322bb04cd68fe09592248b62ffa86ce
Paulo Melo <pauloluziomelo@gmail.com>
2025-09-29 17:20:06 -0700

    Route Django via vercel.json and expose WSGI app

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,14 +1,5 @@
-"""
-WSGI config for api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-app = get_wsgi_application()  # IMPORTANT: variable name is 'app'
+app = get_wsgi_application()  # <-- Vercel looks for 'app'
